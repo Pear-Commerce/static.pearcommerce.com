d07e84bb53808c912d0f3e79480421c9a0a39c08
Alex Wyler <alex.s.wyler@gmail.com>
2020-08-03 14:37:56 -0700

    gulp, gitignore, pear-scripts

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const cp = require('child_process');
+const revision = cp.execSync('git rev-parse --short HEAD').toString().trim();
+const revisionDetails = cp.execSync('git log -n 1 HEAD').toString().trim();
+
+const CONFIGS = {
+    development: {
+        bucket: 'static.pearcommerce.com'
+    },
+    production: {
+        bucket: 'static.pearcommerce.com'
+    }
+};
+
+const config = require('./pear-scripts/configuration.js')(null, null, CONFIGS, null);
+
+gulp.task('deploy', [], function(callback) {
+    require('./pear-scripts/s3-cloudfront-deploy.js')({
+        config: config,
+        cachedSources: ['./**/*'],
+        uncachedSources: [],
+        revision: revision,
+        revisionDetails: revisionDetails,
+        waitForCloudfrontInvalidation: false,
+    }, callback);
+});
