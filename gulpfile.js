7278f68bb578583fdc7240f2461036ddb59a7e10
Alex Wyler <alex.s.wyler@gmail.com>
2020-08-07 13:11:02 -0700

    git-file-include and reusable templates and css

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,34 @@ const gulp = require('gulp');
 const cp = require('child_process');
 const revision = cp.execSync('git rev-parse --short HEAD').toString().trim();
 const revisionDetails = cp.execSync('git log -n 1 HEAD').toString().trim();
+const fileinclude = require('gulp-file-include');
+
+gulp.task('fileinclude', function() {
+    return gulp.src(['./static/*.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: './static/'
+        })).on('error', console.log)
+        .pipe(gulp.dest('./dist/'));
+});
 
 const CONFIGS = {
     development: {
-        bucket: 'static.pearcommerce.com'
+        bucket: 'static.pearcommerce.com',
+        slack: false
     },
     production: {
-        bucket: 'static.pearcommerce.com'
+        bucket: 'static.pearcommerce.com',
+        slack: false
     }
 };
 
 const config = require('./pear-scripts/configuration.js')(null, null, CONFIGS, null);
 
-gulp.task('deploy', [], function(callback) {
+gulp.task('deploy', ['fileinclude'], function(callback) {
     require('./pear-scripts/s3-cloudfront-deploy.js')({
         config: config,
-        cachedSources: ['./static/**/*'],
+        cachedSources: ['./dist/**/*', './static/**/*', '!./static/*.html'],
         uncachedSources: [],
         revision: revision,
         revisionDetails: revisionDetails,
